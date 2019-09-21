cec616e048bcbe4b405a5713500391b81ba771a5
Diogo Amorim <diogoamorim27@gmail.com>
2019-09-21 16:00:42 -0300

    add feed no json, cria json

---
+++ add_feed.pl
@@ -0,0 +1,115 @@
+use strict;
+use warnings;
+use Text::Unaccent::PurePerl qw(unac_string);
+use JSON;
+use File::Slurper;
+
+sub normalize_string
+{ 
+	my $string = $_[0]; 
+
+	my $normalized = unac_string("UTF-8", $string);
+
+	$normalized =~ s/ /_/g;
+	$normalized = lc $normalized;
+
+	return $normalized; 
+}
+
+sub parse_feed
+{
+	my $file_path = $_[0];      #string with the path to *.xml file
+	my %feed_data; #= %{$_[1]};  #reference to hash with feed data
+
+	open (FEED, $file_path)
+		or die "Cant open < $file_path \n";
+	
+	my $feed = "";
+
+	while (my $reader = <FEED>)
+	{
+		$feed = $feed.$reader;
+	}
+
+	($feed_data{title})= $feed =~ /<title>(.*?)<\/title>/;
+
+	($feed_data{language})= $feed =~ /<language>(.*?)<\/language>/;
+
+	foreach my $key (keys %feed_data)
+	{
+		if ($feed_data{$key} =~ /\Q<![CDATA[\E/)
+		{
+			($feed_data{$key}) = $feed_data{$key} =~ /\Q<![CDATA[\E(.*)\Q]]>\E/;
+		}
+	}
+
+	close(FEED);
+
+	return %feed_data;
+}
+
+sub add_feed
+{
+	my $url = $_[0];
+	my $temp_file_path = $_[1];
+
+	my %feed_data;
+
+	%feed_data = parse_feed($temp_file_path, \%feed_data);
+	$feed_data{url} = $url;
+	my $normalized_title = normalize_string($feed_data{title});
+
+	my $cli_command = "mkdir .feeds/".$normalized_title;
+	system($cli_command); #create feed directory
+
+	$cli_command = "mkdir .feeds/".$normalized_title."/eps";
+	system($cli_command); #create downloaded episodes directory
+
+	$cli_command = "mv ".$temp_file_path." .feeds/".$normalized_title."/".$normalized_title.".xml";
+	system($cli_command); #changes temporary *.xml file to correct name and moves it to feed directory;
+
+	return %feed_data;
+}
+
+sub create_feeds_json
+{
+
+	my @empty_array;
+	my $json_empty_array = to_json \@empty_array;
+	if (-e "feeds.json") {
+		system("rm feeds.json");
+	}
+	open(my $fh, ">", "feeds.json") or die "cant open feeds.json";
+	print $fh $json_empty_array;
+	close($fh);
+
+}
+sub append_feed
+{
+	my $feeds_file_path = "feeds.json";
+	my $new_feed_json;
+	
+	my $file_contents = File::Slurper::read_text($feeds_file_path);
+
+	open (my $fh, '>', $feeds_file_path)
+		or die "Cant open < $feeds_file_path! \n ";
+
+	my %new_feed_hash = @_;
+
+	my @file_content_array = @{decode_json($file_contents)};
+
+	push @file_content_array, {%new_feed_hash};
+
+	my $new_file_content = to_json \@file_content_array, {pretty => 1} ;
+
+	print $fh $new_file_content;
+
+		
+}
+
+#programa de teste
+#
+#
+create_feeds_json();
+my %new_feed = parse_feed("./waypoint_radio.xml");
+append_feed(%new_feed);
---
+++ get_episodes.pl
@@ -0,0 +1,56 @@
+use strict;
+use warnings;
+
+sub get_episodes
+{
+	my $file_path = $_[0];      #string with the path to *.xml file
+
+	open (FEED, $file_path);
+
+	my $feed = "";
+
+	while (my $reader = <FEED>)
+	{
+		$feed = $feed.$reader;
+	}
+
+	my @episodes;
+
+	(my @episodes_data_raw) = $feed =~ /<item>(.*?)<\/item>/gs;
+
+	foreach my $episode (@episodes_data_raw)
+	{
+		my %episode_data;
+		($episode_data{title}) = $episode =~ /<title>(.*?)<\/title>/;
+		($episode_data{date}) = $episode =~ /<pubDate>(.*?)<\/pubDate>/;
+		($episode_data{url}) = $episode =~ /<enclosure(.*?)\/>/;
+		($episode_data{url}) = $episode_data{url} =~ /url="(.*?)"/;
+
+		foreach my $key (keys %episode_data)
+		{
+			if ($episode_data{$key} =~ /\Q<![CDATA[\E/)
+			{
+				($episode_data{$key}) = $episode_data{$key} =~ /\Q<![CDATA[\E(.*)\Q]]>\E/;
+			}
+		}
+
+		unshift @episodes, \%episode_data;
+	}
+
+	close(FEED);
+
+	return @episodes;
+}
+
+#programa de teste
+my @episodes = get_episodes(".feeds/hello_internet/hello_internet.xml");
+
+for my $i (0 .. $#episodes)
+{
+	#	for my $attribute ( keys %{$episodes[$i]} )
+	#{
+	#	print "$episodes[$i]{$attribute}\n";
+	#}
+	
+	print "$episodes[$i]{title}\n";
+}
