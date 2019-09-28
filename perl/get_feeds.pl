3e3a82471a7e4a88e253b536d3fd77068d8d066c
mateuspedrosa <mateusmacpedrosa@gmail.com>
2019-09-28 15:34:16 -0300

    added comments on get_feeds.pl

--- perl/get_feeds.pl
+++ perl/get_feeds.pl
@@ -15,19 +15,19 @@ sub get_feeds
         $json = $json.$reader;
     }
 
-    my @feeds;
+    my @feeds;  #cria array de hashes de feeds
     my @feeds_data_raw;
 
-    ($feeds_data_raw[0]) = $json =~ /{(.*)}/s;
-    (@feeds_data_raw) = $feeds_data_raw[0] =~ /{(.*?)}/gs;
+    ($feeds_data_raw[0]) = $json =~ /{(.*)}/s;  #guarda tudo que ta entre a primeira e ultima chave na primeira posicao do array
+    (@feeds_data_raw) = $feeds_data_raw[0] =~ /{(.*?)}/gs;  #percorre a string na primeira posicao e, para cada par "{}", pega o que ha dentro e guarda numa posicao do array
 
     foreach my $feed (@feeds_data_raw)
     {
         my %feed_data;
-        ($feed_data{url}) = $feed =~ /"url":\s+"(.*?)",/;
+        ($feed_data{url}) = $feed =~ /"url":\s+"(.*?)",/;   #$fees_data{url} recebe o que esta entre "\"url\":\"" e "\"," ignorando espacos em branco (\s+)
         ($feed_data{title}) = $feed =~ /"title":\s+"(.*?)",/;
         ($feed_data{language}) = $feed =~ /"language":\s+"(.*?)"/;
-        unshift @feeds, \%feed_data;
+        unshift @feeds, \%feed_data;    #guarda o hash do feed na primeira posicao de @feeds, deslocando os outros elementos para a direita
     }
 
     close($fh)
