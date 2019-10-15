f2f684e512b8165e4478fd565b9bac86cfc5eb78
Diogo Amorim <diogoamorim27@gmail.com>
2019-10-14 23:33:27 -0300

    add episode function fixed

--- perl/add_episode.pl
+++ perl/add_episode.pl
@@ -3,39 +3,47 @@ use warnings;
 use Text::Unaccent::PurePerl qw(unac_string);
 use JSON;
 use File::Slurper;
+use Data::Dumper;
 
 require "./perl/get_episodes.pl";
 require "./perl/normalize_string.pl";
 
 sub add_episode_to_json
 {
-	my $feed_xml_path = $_[0];
-	my $episode_name = $_[1];   #normalized episode title
+	my $feed_name = $_[0];
+	my %episode_data = %{$_[1]};   
 	my $episodes_json_path = $_[2];
+
+	print Dumper(\%episode_data{"title"});
 
 	my $file_contents = File::Slurper::read_text($episodes_json_path);
 
-	open (my $fh, '>', $episodes_json_path)
-		or die "Can't open < $episodes_json_path: $!\n";
+	my %feeds_in_file;
 
-	my @feed_episodes = get_episodes($feed_xml_path);
+	if ($file_contents eq ""){$file_contents = "{}"};
 
-	my %feeds_in_file  = %{decode_json($file_contents)};
+	eval {%feeds_in_file = %{decode_json($file_contents)}}; 
 
-	for my $i (0 .. $#feed_episodes)
-	{
-		if (!($episode_name cmp normalize_string($feed_episodes[$i]{title}))){	
-			$feeds_in_file{$feed_xml_path}{$episode_name} = $feed_episodes[$i];
-		};
-	};
+	if (!($@ eq "")) {
+		return "error!";
+	} else {
 
-	my $new_file_content = to_json \%feeds_in_file, {pretty => 1};
+		my %feeds_in_file = %{decode_json($file_contents)};
 
-	#print $fh $new_file_content;
-
-	close($fh)
-		|| warn "$episodes_json_path - close failed: $!\n";
+		open (my $fh, '>', $episodes_json_path)
+			or die "Can't open < $episodes_json_path: $! \n ";
 	
-} 
+		$feeds_in_file{$feed_name}{$episode_data{"title"}} = \%episode_data;
+
+		#print Dumper(\$feeds_in_file{$feed_name}{$episode_data{"title"}});
+
+		my $new_file_content = to_json \%feeds_in_file, {pretty => 1};
+
+		print $fh $new_file_content;
+#
+		close($fh)
+			|| warn "$episodes_json_path - close failed: $!\n";
+	}
+}
 
 1;
--- perl/add_episode_tester.pl
+++ perl/add_episode_tester.pl
@@ -7,6 +7,12 @@ use File::Slurper;
 require "./perl/add_episode.pl";
 require "./perl/normalize_string.pl";
 
+my %episode_data = (
+    "title" => "Decrepitos 230 - Respondendo Testes da Capricho 5",
+    "date" => "Mon, 16 Sep 2019 17:30:00 -0300",
+    "url" => "https://media.blubrry.com/decrepitos/decrepitos.com/podcast/2019/decrepitos_230_capricho_5.mp3"
+);
+
 my $normalized_title = normalize_string("Decrépitos 225 - VACILO NEWS: Bandido, Polícia e Cachorrinhos");
 
-add_episode_to_json("decrepitos", $normalized_title, "episodes.json");
+print add_episode_to_json("decrepitos", \%episode_data, "episodes_broken.json");
